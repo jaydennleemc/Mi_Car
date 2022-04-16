dbde8e6f1afec411f5a519e1bba5fd97584a28a2
jayden <jaydennleemc@gmail.com>
2022-04-16 13:01:50 +0800

    add router

---
+++ mi_car_app/src/views/controlView.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native'
+
+const ControlView = () => {
+  return (
+    <View>
+      <Text>ControlView</Text>
+    </View>
+  )
+}
+
+
+export default ControlView
---
+++ mi_car_app/src/views/splashView.js
@@ -0,0 +1,19 @@
+import React, { Component, useEffect } from 'react';
+import { View, Text } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context"
+
+
+const SplashView = () => {
+  useEffect(() => {
+
+  }, [])
+
+  return (
+    <View style={{ flex: 1 }}>
+      <SafeAreaView />
+      <Text>SplashView</Text>
+    </View>
+  )
+}
+
+export default SplashView
